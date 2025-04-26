a146f1df32c3d8f969ad7ca9c00e4f9ad4d5d7d0
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-04-26 15:45:47 +0600

    site link updated

--- Server/src/app.js
+++ Server/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ["https://rrfirstchatapp.netlify.app"],
+  origin: ["http://localhost:5173", "https://rrfirstchatapp.netlify.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
