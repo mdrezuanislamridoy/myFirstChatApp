fe7b92c9a1cf588cccca4d5e067892348795f347
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-04-26 18:31:53 +0600

    link

--- Client/src/context/useSendMessage.jsx
+++ Client/src/context/useSendMessage.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import useConversation from "../stateManagement/useConversation";
 import AxiosInstance from "../Apis/AxiosInstance";
 import { useSocket } from "./SocketContext";
+import { AuthContext } from "./MessengerContext";
 
 export default function useSendMessage() {
   const { setMessages, selectedConversation } = useConversation();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const { socket } = useSocket();
+  const { user } = useContext(AuthContext);
 
   const handleSendMessage = async (text) => {
     if (!selectedConversation) return;
@@ -20,7 +22,11 @@ export default function useSendMessage() {
 
       const savedMessage = res.data.messageData;
 
-      socket.emit("sendMessage", savedMessage);
+      socket.emit("sendMessage", {
+        ...savedMessage,
+        sender: user.id,
+        receiver: selectedConversation._id,
+      });
     } catch (err) {
       console.error(err);
       setError("Failed to send message");
