559a80921c95838ad745136b996cf357bed2264e
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-04-26 18:06:33 +0600

    link

--- Client/src/Apis/AxiosInstance.js
+++ Client/src/Apis/AxiosInstance.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const url =
+  import.meta.env.MODE === "production"
+    ? "https://my-first-chat-app-lemon.vercel.app/api"
+    : "http://localhost:5000/api";
+
 const AxiosInstance = axios.create({
-  baseURL: "https://my-first-chat-app-lemon.vercel.app/api",
+  baseURL: url,
   withCredentials: true,
 });
 
